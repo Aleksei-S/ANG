9136b4e00c96bbdc08cd7bec0946d30fc1870198
Aleksei-S <mz-43@mail.ru>
2017-05-02 00:14:52 +0300

    ver 2.0

--- controller.js
+++ controller.js
@@ -1,154 +1,155 @@
-
-var StrokaArray=[];
-var bilOper=false;
 elem = document.getElementById("elem");
 elem.focus();
+var operatorCONST ="";
+var result = makeResult ();    //объект 
 
 
+function makeResult () {
 
+  let firstNumber = 0;
+  let secondNumber = 0;
+  let count = 0;         //счетчик 
 
-function C() {
-	elem.value="";
-	StrokaArray=[];
-	bilOper=false;
+  return {
+    newNumber :  false,
+    operator : "",       //оператор временный
+
+    setNumber : function(value) {  //устанавливает первое или второе чисо
+      count++;
+      if (count == 1) {
+        firstNumber = parseFloat(value);
+      } else {
+        secondNumber = parseFloat(value);
+      }
+    },
+    calculate : function() {       //считает
+      firstNumber = totalCalculate(firstNumber,operatorCONST,secondNumber);
+      return firstNumber;
+    },
+    refresh : function() {         //сброс
+      firstNumber = 0;
+      secondNumber = 0;
+      count = 0;
+    }
+  };
 }
 
-function butNomer(nomer) {
-	if (bilOper==true){
-		elem.value=nomer;
-	}else if (elem.value[0]==0&&elem.value[1]!==","){
-		elem.value=nomer;
-	}else {
-		elem.value=elem.value+nomer;
-	}
-	bilOper=false;
+function totalCalculate (firstNumber, operatorCONST, secondNumber){
+
+  if (operatorCONST == "+") {
+    firstNumber = firstNumber + secondNumber;
+  } 
+  if (operatorCONST == "-") {
+   firstNumber = firstNumber - secondNumber;
+  } 
+  if (operatorCONST == "/") {
+  firstNumber = firstNumber / secondNumber;
+  } 
+  if (operatorCONST == "*") {
+  firstNumber = firstNumber * secondNumber;
+  } 
+  if (operatorCONST=="%") {
+  firstNumber = firstNumber * secondNumber / 100;
+  }
+  if (operatorCONST=="=") {
+    result.refresh();
+  }
+
+  firstNumber=+firstNumber.toFixed(10);
+  return firstNumber;
 }
 
+// клик оператор
+function btnClickOperator (value) {
 
-function butRazdelitel(Razdelitel) {
-	if (elem.value.indexOf(",")!==-1) {
-		return;
-	} else if (elem.value=="") {
-		elem.value="0"+Razdelitel;
-		return;
-	}
-	elem.value=elem.value+Razdelitel;	
+  result.operator = value;
+
+  if (!result.newNumber) {
+    result.setNumber(elem.value);
+    elem.value = result.calculate();  //yyyb
+    result.newNumber = true;
+  }
 }
 
+// клик номер
+function btnClickNumber (value) {
 
-
-function SmenaZnaka(gg) {
-	if (elem.value[0]=="0"||elem.value[0]=="-"&&elem.value[1]=="0"||elem.value=="")
-		{return;}
-	if (elem.value[0]=="-") {
-		elem.value=elem.value.substring(1);
-	} else {
-		elem.value="-"+elem.value;
-		StrokaArray.shift();
-		StrokaArray.unshift(parseFloat(elem.value.replace(/[,]/, '.')));
-	}	
-
+  if (!checkNumber(value)) return;
+  operatorCONST = result.operator;
+  if (!result.newNumber) {
+    elem.value += value;
+  } else {
+    elem.value = value;
+    result.newNumber = false;
+  }
 }
 
+// проверка на число и точку
+function checkNumber (value) {
 
-function Operator(oper) {
-	if (bilOper==true){
-		StrokaArray.pop();
-		StrokaArray.push(oper);
-	}else {
-		if (elem.value==NaN||elem.value=="") {
-			elem.value=0;
-		} 
-		StrokaArray.push(parseFloat(elem.value.replace(/[,]/, '.')));
-		StrokaArray.push(oper);
-	}
-	// Stroka=StrokaArray.join('');
-	// console.log(Stroka);
-	bilOper=true;
+  if (/[0-9]|\./.test(value)) {
 
-	if (StrokaArray.length>=3) {
-		elem.value=pos4itat(StrokaArray[1]);
-	}
-	if (oper=="%") {
-		elem.value=StrokaArray[0]=elem.value/100;
-	}
+    //проверка на точку
+    if (value == ".") {
+      if (elem.value == "" || result.newNumber == true) {
+        elem.value = "0";
+        result.newNumber = false;
+        return true;
+      }else if (elem.value.indexOf(".") == -1) {
+        return true; 
+      } else{
+      return false;
+      }
+    }
 
+    //проверка на ноль
+    if (elem.value[0] == "0" && elem.value.length == 1) {
+      elem.value = elem.value.slice(1, 0);
+    return true;
+    }
+  return true;
+  } else {
+  return false;
+  }
 }
 
+   // смена знака
+function changeSign (value) {
 
+  if (elem.value.substr(0,1) == "-") {
+     elem.value=elem.value.substring(1);
+  }else {
+    elem.value="-"+elem.value;
+  }
 
-function pos4itat(op) {
-	let F="";
-	switch (op) {
-		case "+":
-		F=StrokaArray[0]+StrokaArray[2];	
-		break;
-		case "-":
-		F=StrokaArray[0]-StrokaArray[2];		
-		break;
-		case "*":
-		F=StrokaArray[0]*StrokaArray[2];		
-		break;
-		case "/":
-		F=StrokaArray[0]/StrokaArray[2];		
-		break;	
-	}
-	F=+F.toFixed(10);
-	StrokaArray.shift();
-	StrokaArray.shift();
-	StrokaArray.shift();
-	StrokaArray.unshift(F);
-	elem.focus();
-	return F;
+  result.refresh();
+  result.setNumber(elem.value);
 }
 
+  // сброс
+function refresh () {
 
-
-function Klava(key) {
-
-	if (bilOper==true){
-		elem.value=key.slice(String(StrokaArray[0]).length);
-	}
-
-key=key.replace(/,/g, '.');
-
-count = 0;
-pos = key.indexOf(".");
-while ( pos != -1 ) {
-   count++;
-   if (count>1) {
-  key=elem.value= key.slice(0, -1);
-   }
-   pos = key.indexOf(".",pos+1);
+  result.refresh();
+  elem.value = "";
+  operatorCONST ="";
 }
 
-var char=key.substr(key.length-1,1);
-if (isNumeric(key)){
-	bilOper=false;
-	
-}else if (char=="+"||char=="-"||char=="/"||char=="*"||char=="=") {
-	 key=elem.value= key.slice(0, -1);
-	Operator(char);
-}else{
-	alert("ЧИСЛО ВВЕДИ");
-	C();
+//ввод с клавиатуры
+function keyPressKeyboard(e) {
+
+  let char = e.key;
+
+  if (e.key == ",") {
+    char = ".";
+  }
+
+  if(checkNumber(char)){
+    btnClickNumber (char);
+  } else if (char == "*" || char == "/" || char == "+" || char == "-") {
+   btnClickOperator (char);
+  } else if (char=="Enter" ) {
+   btnClickOperator ("=");
+  } 
+
+ return false;
 }
-
-}
-
-
-function runScript(event){
-	if(event.keyCode == 13){
-		Klava(elem.value+"=");
-	}
-}
-
-
-
-
-function isNumeric(n) {
-  return !isNaN(parseFloat(n)) && isFinite(n);
-}
-
-
-
